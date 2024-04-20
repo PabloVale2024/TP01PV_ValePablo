a30ee18e13bbd193f22eafdca50a21fd99cfa431
PabloVale2024 <166053449+PabloVale2024@users.noreply.github.com>
2024-04-20 10:31:38 -0300

    Ejerccio 12

--- src/ar/edu/unju/fi/ejercicio12/model/Persona.java
+++ src/ar/edu/unju/fi/ejercicio12/model/Persona.java
@@ -14,13 +14,144 @@ public int calcularEdad(int dia, int mes, int anio) {
 			fechaIngresada.set(anio, mes, dia);
 			int anioIngresado=fechaIngresada.get(Calendar.YEAR);
 			int edad = anioActual-anioIngresado;
+
 	return edad;
 }
 
-public String calcualrSigno(int dia, int mes, int anio) {
-	Calendar fechaIngresada=  Calendar.getInstance();
-	fechaIngresada.set(anio, mes, dia);
-	
-	return 
+public String calcualrSigno(int dia, int mes) {
+//	Calendar fechaIngresada=  Calendar.getInstance();
+	//fechaIngresada.set(anio, mes, dia);
+	 if (mes == 1) {
+         if (dia >= 21) {
+             //acuario
+             return "acuario";
+         } else {
+             //capricornio
+             return "capricornio";
+         }
+     }
+     if (mes == 2) {
+         if (dia >= 19) {
+             return "piscis";
+         } else {
+             return "acuario";
+         }
+     }
+     if (mes == 3) {
+         if (dia >= 20) {
+             //acuario
+             return "aries";
+         } else {
+             //capricornio
+             return "piscis";
+         }
+     }
+     if (mes == 4) {
+         if (dia >= 20) {
+             return "tauro";
+         } else {
+             return "Aries";
+         }
+     }
+     if (mes == 5) {
+         if (dia >= 21) {
+             //acuario
+             return "geminis";
+         } else {
+             //capricornio
+             return "tauro";
+         }
+     }
+     if (mes == 6) {
+         if (dia >= 20) {
+             return "cancer";
+         } else {
+             return "geminis";
+         }
+     }
+     if (mes == 7) {
+         if (dia >= 22) {
+             //acuario
+             return "leo";
+         } else {
+             //capricornio
+             return "cancer";
+         }
+     }
+     if (mes == 8) {
+         if (dia >= 21) {
+             return "virgo";
+         } else {
+             return "leo";
+         }
+     }
+     if (mes == 9) {
+         if (dia >= 22) {
+             //acuario
+             return "libra";
+         } else {
+             //capricornio
+             return "virgo";
+         }
+     }
+     if (mes == 10) {
+         if (dia >= 22) {
+             return "escorpion";
+         } else {
+             return "libra";
+         }
+     }
+     if (mes == 11) {
+         if (dia >= 21) {
+             //acuario
+             return "sagitario";
+         } else {
+             //capricornio
+             return "escorpion";
+         }
+     }
+     if (mes == 12) {
+         if (dia >= 21) {
+             return "capricornio";
+         } else {
+             return "sagitario";
+         }
+     }
+     return "fecha valida"; 
+}
+public String estacionAnio(int mes, int dia){
+	if (mes == 1 && dia >=1 && dia <=31) {
+        return "Verano";
+	}  else if (mes == 2 && dia>=1 && dia <= 28) {
+            return "verano";	
+	}else if (mes == 3 && dia >=1 && dia <=20){
+		return "Verano";
+	}else if (mes == 3 && dia >=21 && dia <=31){
+	            return "Otoño";
+	}else if (mes == 4 && dia >=1 && dia <=30){
+		return "Otoño";
+	}else if (mes == 5 && dia >=1 && dia <=31){
+		return "otoño";
+	}else if (mes == 6 && dia >=1 && dia <=20){
+		return "Invierno";
+	}else if (mes == 6 && dia >=21 && dia <=30){
+		return "Invierno";
+	}else if (mes == 7 && dia >=1 && dia <=30){
+		return "Invierno";
+	}else if (mes == 8 && dia >=1 && dia <=31){
+		return "invierno";
+	}else if (mes == 9 && dia >=1 && dia <=20){
+		return "Invierno";
+	}else if (mes == 9 && dia >=21 && dia <=30){
+		return "Primavera";
+	}else if (mes == 10 && dia >=1 && dia <=31){
+		return "Priamvera";	
+	}else if (mes == 11 && dia >=1 && dia <=30){
+		return "Primavera";
+	}else if (mes == 12 && dia >=1 && dia <=20){
+		return "Verano";
+	}else if (mes == 12 && dia >=21 && dia <=31){
+		return "Verano";
+	}return "Fecha Invalida";
 }
 }
