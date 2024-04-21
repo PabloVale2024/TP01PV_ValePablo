e15e465df8f00e9a9648f7eb3a02fc94c32ff4dc
PabloVale2024 <166053449+PabloVale2024@users.noreply.github.com>
2024-04-20 22:47:29 -0300

    ejercicio 15

---
+++ src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio15;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		 Scanner escaneo=new Scanner(System.in);
+		 System.out.println("ingrese un numero entre [5,10]");
+		 numero=escaneo.nextInt();
+		 String [] nombres=new String [numero];
+		 if (numero>4 && numero<11) {
+		 for (int i=0;i<nombres.length;i++) {
+			 System.out.println("ingrese un nombre");
+			 nombres[i]=escaneo.next();
+		 }
+		for (int j=nombres.length-1;j>=0;j--) {			
+			System.out.println("indice: "+j);
+			System.out.println("nombre en Array: "+ nombres[j]);
+			
+		}
+
+	}else {
+		System.out.println("Numero fuera de Rango");
+	}
+	}
+
+}
